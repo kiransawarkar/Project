90962ce164a271fc28cdba8bbe24ff338f92b191
kiransawarkar <kiranmujmule05@gmail.com>
2022-10-20 22:41:07 +0530

    Linear regression model training file added

---
+++ LinearReg/config.py
@@ -0,0 +1,8 @@
+import os
+
+CSV_FILE_PATH ='enter_path'
+
+PORT_NUMBER=5005
+
+JSON_FILE_PATH=os.path.join('Project_data','project_data.json')
+MODEL_FILE_PATH=os.path.join('Project_data','linear_model.pkl')
